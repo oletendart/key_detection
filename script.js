226f266c7c3200ace90679ecf81782b656ec0134
Olivia Letendart <letendartolivia@gmail.com>
2020-01-26 17:25:07 +0100

    create splice const in the event listener window

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ const secretCode = "wesbos";
 window.addEventListener("keyup", e => {
   console.log(e.key);
   pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   console.log(pressed);
 });
