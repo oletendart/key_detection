6614c82eb13ed19f51e143e2f1e3f51920516840
Olivia Letendart <letendartolivia@gmail.com>
2020-01-26 17:27:25 +0100

    create loop if sending a message when this happens at the last index

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ window.addEventListener("keyup", e => {
   console.log(e.key);
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join("").includes(secretCode)) {
+    console.log("Ding Ding");
+  }
   console.log(pressed);
 });
